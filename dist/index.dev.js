30acc3b1e846e205483cb91056c6831853a61555
radchenkoed <radchenkoed@sgu.ru>
2023-12-08 00:05:03 +0400

    Updates

--- dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-console.log(el);
-
-window.onresize = function () {
-  document.querySelectorAll('input[type="checkbox"]:checked').forEach(function (el) {
-    el.checked = false;
-  });
-};
