060f01112208dc82b3000b0d8de08bee0dcec355
radchenkoed <radchenkoed@sgu.ru>
2023-12-08 01:48:33 +0400

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onresize = () => { // to skip all checked chekboxes (burger, load more)
-    document.querySelectorAll('input[type="checkbox"]:checked').forEach((el) => {
+// window.onresize = () => { // to skip all checked chekboxes (burger, load more)
+//     document.querySelectorAll('input[type="checkbox"]:checked').forEach((el) => {
         
-        el.checked = false
-    })
-}
+//         el.checked = false
+//     })
+// }
